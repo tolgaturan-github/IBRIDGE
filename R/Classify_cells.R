7d3fdd69e56322682b5ed326c1ca747d96522409
Tolga Turan <tolga.turan@abbvie.com>
2022-10-28 17:57:37 +0000

    README update

--- R/Classify_cells.R
+++ R/Classify_cells.R
@@ -1,6 +1,6 @@
 #' Classify single-cells or in vitro cell lines
 #' Classifies the input single-cell or in vitro cell line data into "inflamed", "cold" or "unassigned" bins
-#' @param seu1 Output of UMI count matrix normalized by Seurat workflows
+#' @param seu1 Output of UMI count matrix normalized by Seurat workflows 
 #' @param IBRIDGE_features Output of IBRIDGE_overlaps function.
 #' @param norm_method "SCTransform" or "NormalizeData". Defaults to "SCTransform"
 #' @param n_cores Integer of length 1. Specifies the number of parallel cores running the function.
@@ -20,7 +20,7 @@ Classify_cells<-function(seu1, IBRIDGE_features,norm_method="SCTransform", n_cor
 	nes1<-sc_geneset_enrich(seu1, IBRIDGE_features, norm_method, n_cores)
 	nes_df<-data.frame(nes1[,1:2], Inflamed_tertiles=assign_tertiles(nes1[,1], "Inflamed"), Cold_tertiles=assign_tertiles(nes1[,2], "Cold"))
 	nes_df$Class<-ifelse(nes_df$Inflamed_tertiles=="Inflamed_high"&nes_df$Cold_tertiles!="Cold_high", "Inflamed", ifelse(nes_df$Cold_tertiles=="Cold_high"&nes_df$Inflamed_tertiles!="Inflamed_high", "Cold", "Unassigned"))
-	seu1@meta.data$IBRIDGE_Class<-nes_df$Class
+	seu1@meta.data$iBRIDGE_Class<-nes_df$Class
 	seu1@meta.data$iBRIDGE_Score<-nes1[,1]/nes1[,2]
 	seu1}
 
